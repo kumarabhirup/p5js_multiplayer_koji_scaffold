6e47fe9d72b41235d4965edc1b11c42da52ab947
Kumar Abhirup <kumar@iqubex.com>
2019-10-04 19:32:26 +0530

    📦 NEW: Update GameContainerJS with koji-dispatch

--- frontend/app/react/GameContainer.js
+++ frontend/app/react/GameContainer.js
@@ -2,9 +2,10 @@
 /* eslint-disable import/no-webpack-loader-syntax */
 /* eslint-disable global-require */
 
-/* global sndMusic */
+/* global sndMusic dispatch Koji */
 
 import React, { Component } from 'react'
+import Dispatch, { DISPATCH_EVENT } from '@withkoji/dispatch'
 
 const { p5 } = window
 
@@ -28,6 +29,32 @@ class GameContainer extends Component {
     require('script-loader!app/lib/spawn.js')
     require('script-loader!app/lib/entities.js')
 
+    let roomName = Koji.config.strings.defaultRoomName
+    if (localStorage.getItem('roomName')) {
+      roomName = localStorage.getItem('roomName')
+    }
+
+    let _playerName = Koji.config.strings.defaultPlayerName
+    if (localStorage.getItem('playerName')) {
+      _playerName = localStorage.getItem('playerName')
+    }
+
+    // eslint-disable-next-line no-global-assign
+    dispatch = new Dispatch({
+      projectId: Koji.config.metadata.projectId,
+      options: {
+        shardName: roomName, // the name of the shard you want to connect to. if this key is not present, the user will automatically be placed onto a shard
+        maxConnectionsPerShard: parseInt(
+          Koji.config.strings.maxPlayersPerShard
+        ), // specify how many users to allow on a shard before it is "full" -- once a shard is full, new users will be added to a new shard unless a different shard is explicity set
+      },
+    })
+
+    // eslint-disable-next-line no-global-assign
+    dispatchEvent = DISPATCH_EVENT
+
+    dispatch.setUserInfo({ playerName: _playerName })
+
     // eslint-disable-next-line new-cap
     this.p5Game = new p5(null, document.getElementById('game-container'))
   }
@@ -36,6 +63,7 @@ class GameContainer extends Component {
     if (sndMusic && sndMusic.isPlaying()) {
       sndMusic.dispose()
     }
+
     this.p5Game.remove()
   }
 
